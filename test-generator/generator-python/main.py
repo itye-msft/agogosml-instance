da5dc6657bb68184aa96197c56a66d3d2b5bc4cd
Greg Oliver <golive@microsoft.com>
2018-10-11 11:18:27 -0700

    add documentation, invocation of builtin test based on file input

--- test-generator/generator-python/main.py
+++ test-generator/generator-python/main.py
@@ -4,6 +4,7 @@ import json
 import pickle
 import test_sub_builtIn
 from azure.eventhub import EventHubClient, Sender, EventData
+import IPython
 
 def load_config():
     '''
@@ -14,18 +15,61 @@ def load_config():
 
     return config
 
-def load_tests_from_plugins():
-    required_plugins = []
+def get_tests_from_configuration(input_type):
+
+    # get the configuration
     harness_config = load_config()
+
+    # initialize the list of required plugins
+    required_plugins = []
+
+    # get the list of tests from the configuration
     loaded_tests = harness_config['tests']
+
+    # loop through the tests and get the required plugins
     for loaded_test in loaded_tests:
-        if loaded_test['input']['type'] == 'plugin':
+        if loaded_test['input']['type'] == input_type:
             required_plugins.append(loaded_test['assembly'])
+
+    # eliminate duplicates from the list of required plugins
     unique_plugins = list(set(required_plugins))
-    test_classes_in_plugin = []
+
+    # get the test classes in the plugins
+    test_classes_in_plugins = []
     for plugin in unique_plugins:
-        test_classes_in_plugin += pickle.load(open( plugin + ".p", "rb" ))
-    return test_classes_in_plugin
+        test_classes_in_plugins += pickle.load(open( plugin + ".p", "rb" ))
+
+    # make a list of dictionary to return - have to add a couple of attributes
+    # need the input_type for all, filename for file inputs.
+    tests_to_return = []
+    for loaded_test in loaded_tests:       
+        test_typename = loaded_test['type']
+        if (loaded_test['input']['type'] == input_type) and (input_type == 'plugin'):
+            tests_to_return.append(
+                {
+                    'input_type': 'plugin', 
+                    'class': [elem for elem in test_classes_in_plugins if elem.__name__ == test_typename][0]
+                }
+            )
+        elif (loaded_test['input']['type'] == input_type) and (input_type == 'file'):
+            tests_to_return.append(
+                {
+                    'input_type': 'file', 
+                    'class': [elem for elem in test_classes_in_plugins if elem.__name__ == test_typename][0],
+                    'filename': loaded_test['input']['filename']
+                }
+            )
+
+    # return that list to the caller
+    return tests_to_return
+
+def load_tests_from_plugins():
+
+    tests_in_plugins = get_tests_from_configuration('plugin')
+    tests_in_plugins += get_tests_from_configuration('file')
+ 
+    # return that list to the caller
+    return tests_in_plugins
 
 def invoke_tests():
     '''
@@ -48,8 +92,11 @@ def invoke_tests():
 
     try:
         for test in loaded_tests:
-            instance = test()
-            sender.send(EventData(instance.generate()))
+            instance = test['class']()
+            if test['input_type'] == 'plugin':
+                sender.send(EventData(instance.generate()))
+            elif test['input_type'] == 'file':
+                sender.send(EventData(instance.generate(test['filename'])))
     except Exception as e:
         raise e       
     finally:
